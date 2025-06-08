5df88bceb5af5c6621c922170dc40686d60a57e6
Simone Brunelli <75955476+Purp7ePi3@users.noreply.github.com>
2025-06-09 00:24:27 +0200

    Delete OOP24-UniversityEscape directory

--- OOP24-UniversityEscape/settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    // Automatically downloads the correct java version to run the static analyzers
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-}
-
-rootProject.name = "sample-gradle-project"
